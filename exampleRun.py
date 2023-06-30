b06f81454ed27f44d6962ae59a49a470e6219c6d
Hannah Nelson <nexec64@gmail.com>
2023-06-29 20:03:29 -0700

    tested it! it works!

--- exampleRun.py
+++ exampleRun.py
@@ -2,8 +2,11 @@ from fluxCenter import fluxCenter
 from Vector2D import Vector2D as v2
 print(
     fluxCenter([
-        v2(0,0),
         v2(0,2),
-        v2(1,0),
+        v2(5,0),
+        v2(-1,-3),
+        v2(-1,-2),
+        v2(-2,-1),
+        v2(-1,1)
     ])
 )
